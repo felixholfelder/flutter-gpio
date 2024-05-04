a3d2b9279e53e28ffa68cda04d82d989d1f0f1f2
Felix Holfelder <felixholfelder1@t-online.de>
2024-05-04 13:12:51 +0200

    added other gpio lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:rpi_gpio/gpio.dart';
-import 'package:rpi_gpio/rpi_gpio.dart';
+
+import 'package:dart_periphery/dart_periphery.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,8 +34,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool _ledState = false;
-  late Gpio _gpio;
-  late GpioOutput _led;
+  // late Gpio _gpio;
+  // late GpioOutput _led;
 
   @override
   void initState() {
@@ -64,24 +66,48 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   toggleLed() {
-    print("toggle");
-    try {
-      setState(() {
-        _ledState = !_ledState;
-        _led.value = _ledState;
-      });
-    } catch (e) {
-      print("Initialization not finished");
-      print(e);
-    }
+    // print("toggle");
+    // try {
+    //   setState(() {
+    //     _ledState = !_ledState;
+    //     _led.value = _ledState;
+    //   });
+    // } catch (e) {
+    //   print("Initialization not finished");
+    //   print(e);
+    // }
   }
 
   initGpio() async {
-    _gpio = await initialize_RpiGpio();
-    setState(() {
-      _gpio;
-      _led = _gpio.output(14)..value = false;
-    });
-    print("Initialization finished!");
+    // _gpio = await initialize_RpiGpio();
+    // setState(() {
+    //   _gpio;
+    //   _led = _gpio.output(14)..value = false;
+    // });
+    // print("Initialization finished!");
+
+
+    var config = GPIOconfig.defaultValues();
+    config.direction = GPIOdirection.gpioDirOut;
+    print('Native c-periphery Version :  ${getCperipheryVersion()}');
+    print('GPIO test');
+    var gpio = GPIO(14, GPIOdirection.gpioDirOut);
+
+    print('GPIO info: ${gpio.getGPIOinfo()}');
+
+    print('GPIO native file handle: ${gpio.getGPIOfd()}');
+    print('GPIO chip name: ${gpio.getGPIOchipName()}');
+    print('GPIO chip label: ${gpio.getGPIOchipLabel()}');
+    print('GPIO chip name: ${gpio.getGPIOchipName()}');
+    print('CPIO chip label: ${gpio.getGPIOchipLabel()}');
+
+    for (var i = 0; i < 10; ++i) {
+      gpio.write(true);
+      sleep(Duration(milliseconds: 200));
+      gpio.write(false);
+      sleep(Duration(milliseconds: 200));
+    }
+
+    gpio.dispose();
   }
 }
